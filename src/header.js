28f92226630639ae5502d6cc437156345c1739f5
KrzysztofKruk <kr.kruk@gmail.com>
2023-05-31 13:53:28 +0200

    First working version

---
+++ src/header.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Data Filter-Splitter
+// @namespace    KrzysztofKruk-FlyWire
+// @version      0.1
+// @description  Helps processing large amounts of segments by filtering and splitting them into smaller batches
+// @author       Krzysztof Kruk
+// @match        https://ngl.flywire.ai/*
+// @match        https://edit.flywire.ai/*
+// @connect      prodv1.flywire-daf.com
+// @updateURL    https://raw.githubusercontent.com/ChrisRaven/FlyWire-Data-Filter-Splitter/main/Data-filter-splitter.user.js
+// @downloadURL  https://raw.githubusercontent.com/ChrisRaven/FlyWire-Data-Filter-Splitter/main/Data-filter-splitter.user.js
+// @homepageURL  https://github.com/ChrisRaven/FlyWire-Data-Filter-Splitter
+// ==/UserScript==
